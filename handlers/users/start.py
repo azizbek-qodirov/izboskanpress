234dabcc9e894940d6200b2f9ea976c823a1e00e
Azizbek Qodirov <qodirovazizbek249@gmail.com>
2023-06-05 13:16:05 +0500

    1.1.0

--- handlers/users/start.py
+++ handlers/users/start.py
@@ -7,6 +7,7 @@ from data.config import DEVID
 allStarts = 0
 @dp.message_handler(CommandStart())
 async def bot_start(message: types.Message):
+  global allStarts
   allStarts = allStarts + 1
   await bot.send_message(chat_id=DEVID, text=f"All starts count: {allStarts}")
   await message.answer(f"Aссалому алайкум {message.chat.first_name} Избоскан инфо ботига хуш келибсиз! \nБотдан фойдаланиш учун қуйидаги тугмалардан фойдаланишингиз мумкин: ", reply_markup=menuKeyboard)

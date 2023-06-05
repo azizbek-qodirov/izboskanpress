9aec986b98216d1a9237b336c1c3c4d96826f070
Azizbek Qodirov <qodirovazizbek249@gmail.com>
2023-06-05 13:01:07 +0500

    user's start count added

--- handlers/users/start.py
+++ handlers/users/start.py
@@ -1,9 +1,12 @@
 from aiogram import types
 from aiogram.dispatcher.filters.builtin import CommandStart
 from keyboards.default.menuKeys import menuKeyboard
-from loader import dp
+from loader import dp, bot
+from data.config import DEVID
 
-
+allStarts = 0
 @dp.message_handler(CommandStart())
 async def bot_start(message: types.Message):
-    await message.answer(f"Aссалому алайкум {message.chat.first_name} Избоскан инфо ботига хуш келибсиз! \nБотдан фойдаланиш учун қуйидаги тугмалардан фойдаланишингиз мумкин: ", reply_markup=menuKeyboard)
+  allStarts = allStarts + 1
+  await bot.send_message(chat_id=DEVID, text=f"All starts count: {allStarts}")
+  await message.answer(f"Aссалому алайкум {message.chat.first_name} Избоскан инфо ботига хуш келибсиз! \nБотдан фойдаланиш учун қуйидаги тугмалардан фойдаланишингиз мумкин: ", reply_markup=menuKeyboard)
